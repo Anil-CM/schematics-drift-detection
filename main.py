e4e0963067468bea3c47a7d629d57d14ef5a8d8c
anil <anil@anils-MacBook-Pro.local>
2022-09-03 17:13:46 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+def main():
+    print("hello")
+    # 1. Initialise config
+    # Workspace-id, Auth-token/api-key, schematics-endpoint
+    
+    # 2. Run task - 1: Detect drift
+
+    # 3. Run task - 2: Get logs
+
+    # 4. Run task - 3: Analyse logs
+
+    # 5. Run task - 4: Send notification
+
+if __name__ == "__main__":
+    main()
